90b6f75e0de8ad50dfe6c0053fb99e475f990146
Defoiaif <94846680+Defoiaif@users.noreply.github.com>
2025-01-28 10:39:07 +0100

    Use fixture in delete team test

--- python/test_delete_team.py
+++ python/test_delete_team.py
@@ -1,8 +1,8 @@
-def test_delete_team(page):
+from conftest import reset_database, add_employee
+
+def test_delete_team(page, reset_database, add_employee):
     # Make sure db is empty
-    page.goto("/reset_db")
-    proceed_button = page.locator("button:has-text('proceed')")
-    proceed_button.click()
+    reset_database()
 
     # Create a team 
     page.goto("/add_team")
@@ -12,16 +12,7 @@ def test_delete_team(page):
     page.click("text='Add'")
 
     # Create User
-    page.goto("/add_employee")
-    user_name = "User"
-    page.locator('input[name="name"]').fill(user_name)
-    page.locator('input[name="email"]').fill("test@test.tst")
-    page.locator('input[name="address_line1"]').fill("111 avenu de JeSaisPas")
-    page.locator('input[name="city"]').fill("Paris")
-    page.locator('input[name="zip_code"]').fill("75018")
-    page.locator('input[name="hiring_date"]').fill("2024-02-12")
-    page.locator('input[name="job_title"]').fill("Test")
-    page.click("text='Add'")
+    add_employee()
 
     # Add user to a team
     page.goto("/employees")
@@ -38,5 +29,5 @@ def test_delete_team(page):
     # Check the new team is there
     assert not page.is_visible(f"td:has-text('{team_name}')")
     page.goto("/employees")
-    assert page.is_visible(f"td:has-text('{user_name}')")
+    assert page.is_visible(f"td:has-text('test_user')")
 
